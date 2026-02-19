f42192e30d932f4cfd840b82b6b11f646a29ca4d
Mihir Sahoo <mihirsahoo1155.com>
2026-02-20 00:11:51 +0530

    --- Check the build --------

--- src/app/(mainBody)/404/page.jsx
+++ src/app/(mainBody)/404/page.jsx
@@ -4,6 +4,8 @@ import ThemeOptionContext from "@/context/themeOptionsContext";
 import Loader from "@/layout/loader";
 import React, { useContext } from "react";
 
+// Build done 
+
 const PageNotFound = () => {
   const { isLoading } = useContext(ThemeOptionContext);
 
